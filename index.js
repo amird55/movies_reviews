8e28215f3c2615e652e46e8ed2ad228cc4c0c9f1
amird55 <amir.dgn@outlook.com>
2024-11-26 18:58:32 +0200

    הוספת דף קדמי עם טופס להוספת ביקורת

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.json());
 
 const bodyParser = require('body-parser');
+const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
 
 let curr_id=1;
@@ -26,6 +27,9 @@ app.get('/Review', (req, res) => {
     res.status(200).json(reviews);
 });
 
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname,"/views/review_main.html"));
+});
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port http://localhost:${port}`);
