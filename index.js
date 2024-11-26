aba24b07661f5ea5fdd31b33c0d929566b2f729c
amird55 <amir.dgn@outlook.com>
2024-11-26 18:39:39 +0200

    adding endpoint for adding a new review

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ let r1={id:curr_id,title:"LOTR",content:"one to rule"};
 curr_id++;
 reviews.push(r1);
 
+app.post("/Review",(req,res)=>{
+    let item={};
+    item.id      = curr_id;
+    item.title   = req.body.title;
+    item.content = req.body.content;
+    curr_id++;
+    reviews.push(item);
+
+    res.status(200).json("ok");
+});
+
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port http://localhost:${port}`);
